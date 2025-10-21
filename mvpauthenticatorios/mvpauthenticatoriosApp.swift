2104651072271170585f1bb99f37a3594e01fb73
Chris Phua <chris.phua@bunkerchain.io>
2025-10-21 10:33:42 +0800

    Initial Commit

---
+++ mvpauthenticatorios/mvpauthenticatoriosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvpauthenticatoriosApp.swift
+//  mvpauthenticatorios
+//
+//  Created by Chris Phua on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct mvpauthenticatoriosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mvpauthenticatoriosTests/mvpauthenticatoriosTests.swift
@@ -0,0 +1,17 @@
+//
+//  mvpauthenticatoriosTests.swift
+//  mvpauthenticatoriosTests
+//
+//  Created by Chris Phua on 21/10/25.
+//
+
+import Testing
+@testable import mvpauthenticatorios
+
+struct mvpauthenticatoriosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
